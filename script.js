c691c9d0ce8111df394c657dacec9c3332843ee6
Victor Dominguez <vdomim@gmail.com>
2023-08-20 00:30:57 +0200

    Adding the basic operations functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    if (b === 0) return 'Error. Can`t divide by 0'
+    return a / b
+}
